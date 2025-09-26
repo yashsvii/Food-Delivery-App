acecb5a54622a4a704a4acfb6a7aab5569fdf573
Yashsvi Singh <165875571+yashsvii@users.noreply.github.com>
2025-09-26 11:50:16 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import FoodDisplay from '../../components/Fooddisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
